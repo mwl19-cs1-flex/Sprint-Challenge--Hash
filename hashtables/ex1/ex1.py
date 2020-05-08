325f69663a9c1c4962d6db73978f464ea66587d1
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-05-08 11:00:37 -0700

    Commit for retro

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
+    item_1 = None
+    item_2 = None
+    for i in weights:
+        print('i', i)
+        for j in range(1, length):
+            print('j', weights[j])
+            if i + weights[j] == limit:
+                item_1 = i
+                item_2 = j
+                print('hit') 
 
-    """
-    YOUR CODE HERE
-    """
+    return (item_1, item_2)
 
-    return None
+weightlist = [1, 3, 5, 7, 9, 11]
+get_indices_of_item_weights(weightlist, 6, 4)
