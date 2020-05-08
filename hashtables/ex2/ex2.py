847eff307b8ef99b6e5428c0938130c3d45e399d
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-05-08 15:28:32 -0700

    Completed ex3

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,12 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for i in tickets:
+        Ticket(i[0], i[1])
+
+    # if ticket.source is 'None"
+    # it is the start of the trip
+    # if ticket.destination is none
+    # it is the end of the trip
 
     return route
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,33 @@
 def intersection(arrays):
-
+    result = []
+    d = {}
     """
     YOUR CODE HERE
     """
+    for item in arrays[0]:
+        d[item] = 0
 
+    for array in arrays:
+        for item in array:
+            if item in d:
+                d[item] += 1
+            else: 
+                pass
+
+    for item in d: 
+        if d[item] == len(arrays):
+            result.append(item)
+    
     return result
+
+    
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000,2000000)) + [1,2,3])
-    arrays.append(list(range(2000000,3000000)) + [1,2,3])
-    arrays.append(list(range(3000000,4000000)) + [1,2,3])
+    arrays.append(list(range(1,5)) + [1,2,3])
+    arrays.append(list(range(2,6)) + [1,2,3])
+    arrays.append(list(range(3,7)) + [1,2,3])
 
     print(intersection(arrays))
