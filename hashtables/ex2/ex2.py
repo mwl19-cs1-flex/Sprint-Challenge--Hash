e203ad796d90941b588c28ce36aa5f690b6a4e4c
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-05-08 16:11:26 -0700

    Completed ex2, 2 of 3 completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,12 +10,38 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    for i in tickets:
-        Ticket(i[0], i[1])
+    route = []
+    t = {}
 
-    # if ticket.source is 'None"
-    # it is the start of the trip
-    # if ticket.destination is none
-    # it is the end of the trip
+    # Loads the ticket into the hash table
+    for ticket in tickets:
+        t[ticket.source] = ticket.destination
 
+    # Sets the value of the item in T[key="NONE"] to variable
+    current_ticket = t['NONE']
+    # Appends value to route
+    route.append(current_ticket)
+
+    # While this value is not a string of NONE
+    while t[current_ticket] != 'NONE':
+        # for each value of each key in t
+        for item in t:
+            # if the value of current ticket is equal to the key value
+            if t[current_ticket] == item:
+                # add the route of the item
+                route.append(item)
+                # set the current ticket to the item
+                current_ticket = item
+            else:
+                pass
+    # need to append the last route to it
+    route.append(t[current_ticket])
+    
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+result = reconstruct_trip(tickets, 3)
